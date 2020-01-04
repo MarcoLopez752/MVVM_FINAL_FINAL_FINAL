5d3fe2e81216e879cb82deddaeecfb8a21b01b17
BuildTools <unconfigured@null.spigotmc.org>
2020-01-03 20:04:31 -0400

    first commit

---
+++ Xamarin/app/app/Views/Prueba.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace app.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewPage1 : ContentPage
+    {
+        public ObservableCollection<string> Items { get; set; }
+
+        public ListViewPage1()
+        {
+            InitializeComponent();
+
+            Items = new ObservableCollection<string>
+            {
+                "Elemento 1",
+                "Elemento 2",
+                "Elemento 3",
+                "Elemento 4",
+                "Elemento 5"
+            };
+
+            MyListView.ItemsSource = Items;
+        }
+
+        async void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item == null)
+                return;
+
+            await DisplayAlert("Item Tapped", "An item was tapped.", "OK");
+
+            //Deselect Item
+            ((ListView)sender).SelectedItem = null;
+        }
+    }
+}
